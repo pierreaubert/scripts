4f3ac918e3b5f89a0760fdf61dc55cf8f2b60302
Pierre Aubert <pierreaubert@yahoo.fr>
2019-11-02 21:10:24 +0100

    add some basic checks

--- backupEFI.sh
+++ backupEFI.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-RSYNC=/usr/local/bin/rsync 
+MACOS=Mojave
 CLONE=/Volumes/MojaveClone
+
+# need a recent enough version of rsync
+RSYNC=/usr/local/bin/rsync 
 
 if [ $# -ne 2 ]
 then
-  echo "Usage: " $0 "from_disk to_disk";
+  echo "Usage: " $0 "from_partion to_partition";
+  echo "  ex: "$0 " disk0s1 disk3s1"
   exit -1;
 fi
 
@@ -15,9 +19,19 @@ EFI_CLONE=$3
 diskutil unmount $EFI_ROOT
 diskutil unmount $EFI_CLONE
 
+if test -d /Volumes/EFI; then
+  echo "Error root EFI at "$EFI_ROOT"is still mounted!"
+  exit 1
+fi
 diskutil mount $EFI_ROOT
+
+if test -d /Volumes/EFI\ 1; then
+  echo "Error root EFI\ 1 at "$EFI_CLONE"is still mounted!"
+  exit 1
+fi
 diskutil mount $EFI_CLONE
 
 # sync / to clone
+# -n is fake remove it to actually copy files
 ${RSYNC} -nxrlptgoXvHS --exclude '*.dontcopy' --delete --fileflags /Volumes/EFI/EFI /Volumes/EFI\ 1/EFI
 
